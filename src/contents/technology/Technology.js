6936d53e278a09db622392e1bd9f748abc74f3d5
ahmadsubekhi <ahmadsubekhi550@gmail.com>
2022-09-07 13:38:30 +0800

    update

--- src/contents/technology/Technology.js
+++ src/contents/technology/Technology.js
@@ -21,12 +21,12 @@ const Technology = () => {
         03 Space launch 101
       </h1>
       <div className=" grid grid-cols-1 lg:grid-cols-2 max-w-5xl mx-auto py-6 lg:py-10 ">
-        <div className=" order-2 lg:order-1 flex gap-8 justify-center items-center rounded-xl relative">
-          <div className="flex flex-col gap-y-8 text-white pt-28 lg:pt-0 pl-6 lg:pl-0 text-2xl">
+        <div className="order-2 lg:order-1 flex flex-col lg:flex-row lg:gap-8  rounded-xl relative py-8 lg:py-0">
+          <div className="flex flex-row lg:flex-col justify-center gap-8 lg:gap-y-8 text-white lg:pt-0 lg:pl-0 text-2xl">
             <button
               className={`${
                 techImg === 0 ? "active" : ""
-              } border-1 rounded-full w-10 h-10 lg:w-20 lg:h-20 focus:backdrop-blur-md hover:bg-white/80 hover:text-black focus:bg-white/80 focus:text-black outline `}
+              } border-1 rounded-full w-16 h-16 lg:w-20 lg:h-20 focus:backdrop-blur-md hover:bg-white/80 hover:text-black focus:bg-white/80 focus:text-black outline `}
               onClick={() => {
                 setTechImg(0);
               }}
@@ -36,7 +36,7 @@ const Technology = () => {
             <button
               className={`${
                 techImg === 1 ? "active" : ""
-              }border-1 rounded-full w-10 h-10 lg:w-20 lg:h-20 focus:backdrop-blur-md hover:bg-white/80 hover:text-black focus:bg-white/80 focus:text-black outline`}
+              }border-1 rounded-full w-16 h-16 lg:w-20 lg:h-20 focus:backdrop-blur-md hover:bg-white/80 hover:text-black focus:bg-white/80 focus:text-black outline`}
               onClick={() => {
                 setTechImg(1);
               }}
@@ -46,7 +46,7 @@ const Technology = () => {
             <button
               className={`${
                 techImg === 2 ? "active" : ""
-              }border-1 rounded-full w-10 h-10 lg:w-20 lg:h-20 focus:backdrop-blur-md hover:bg-white/80 hover:text-black focus:bg-white/80 focus:text-black outline`}
+              }border-1 rounded-full w-16 h-16 lg:w-20 lg:h-20 focus:backdrop-blur-md hover:bg-white/80 hover:text-black focus:bg-white/80 focus:text-black outline`}
               onClick={() => {
                 setTechImg(2);
               }}
@@ -55,7 +55,7 @@ const Technology = () => {
             </button>
           </div>
 
-          <div className="flex flex-col text-justify pr-6 lg:pr-0 text-white space-y-6 pt-8 lg:pt-0">
+          <div className="flex flex-col text-justify items-center lg:items-start justify-center px-6 lg:px-0 lg:pr-0 text-white space-y-6 pt-8 lg:pt-0">
             <h2 className=" text-xl font-exo text-slate-400">{page.termin}</h2>
             <h1 className=" uppercase font-exo text-4xl">{page.tech_name}</h1>
             <p className=" font-exo">{page.tech_text}</p>
--- src/contents/technology/TechnologyData.js
+++ src/contents/technology/TechnologyData.js
@@ -2,7 +2,7 @@ export const TechData = [
   {
     id: 0,
     number: 1,
-    termin: "THE TERMINOLOGY…",
+    termin: "THE TERMINOLOGY",
     tech_name: "Launcher Vechile",
     tech_text:
       "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
@@ -10,7 +10,7 @@ export const TechData = [
   {
     id: 1,
     number: 2,
-    termin: "THE TERMINOLOGY…",
+    termin: "THE TERMINOLOGY",
     tech_name: "Spaceport",
     tech_text:
       "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
@@ -18,7 +18,7 @@ export const TechData = [
   {
     id: 2,
     number: 3,
-    termin: "THE TERMINOLOGY…",
+    termin: "THE TERMINOLOGY",
     tech_name: "Space Capsule",
     tech_text:
       "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
