940b070ded7101e2cf712c8796a72d280b19ae1d
ahmadsubekhi <ahmadsubekhi550@gmail.com>
2022-09-07 12:41:24 +0800

    home update

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -3,27 +3,26 @@ import NavigasiBar from "./NavigasiBar";
 
 const Home = () => {
   return (
-    <div id="home" className="bg-hero w-full h-full py-4">
+    <div id="des" className=" bg-hero py-4 w-full h-full">
       <NavigasiBar />
-      <div className=" box-border mx-auto max-w-5xl text-white grid grid-cols-1 lg:grid-cols-2 lg:py-2 ">
-        <div className=" order-2 lg:order-1 flex items-center justify-center lg:justify-start py-8 lg:py-20 ">
-          <div className=" space-y-5 font-exo w-11/12">
-            <h1 className=" flex  items-center justify-center lg:justify-start max-w-5xl mx-auto text-white text-2xl tracking-widest font-exo py-4">
-              So, you want to travel to
-            </h1>
-            <h1 className=" flex  items-center justify-center lg:justify-start font-semibold lg:text-left text-8xl lg:text-9xl tracking-widest uppercase">
-              Space
-            </h1>
-            <p className=" lg:text-left leading-4 text-base text-justify text-slate-300">
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we’ll give you a truly out of
-              this world experience!
-            </p>
-          </div>
+
+      <div className=" grid grid-cols-1 lg:grid-cols-2 max-w-5xl mx-auto py-6 lg:pt-20 ">
+        <div className=" order-2 lg:order-1 text-white font-exo space-y-4">
+          <h1 className=" flex justify-center lg:justify-start font-exo text-2xl uppercase">
+            So, you want to travel to
+          </h1>
+          <h1 className=" flex justify-center lg:justify-start font-exo text-9xl uppercase">
+            Space
+          </h1>
+          <p className=" text-justify lg:text-left font-exo px-4 lg:px-0">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
         </div>
-        <div className=" order-1 lg:order-2 flex justify-center items-center">
-          <div className=" font-exo tracking-widest flex justify-center cursor-pointer hover:scale-125 hover:opacity-50 duration-300 items-center h-52 w-52 rounded-full bg-white text-black uppercase">
+        <div className=" order-1 lg:order-2 flex items-center justify-center py-8 lg:py-0">
+          <div className=" font-exo tracking-widest flex justify-center cursor-pointer hover:scale-125 hover:opacity-50 duration-300 items-center h-72 w-72 rounded-full bg-white text-black uppercase">
             <a href="#" className=" animate-spin-slow2">
               Explore
             </a>
